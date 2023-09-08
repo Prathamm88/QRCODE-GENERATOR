4d5acf117d030871359eacc9781b1bb57d2a6ecb
Pratham_Pundir <124234938+Prathamm88@users.noreply.github.com>
2023-09-08 20:10:23 +0530

    Update pratham.py

--- pratham.py
+++ pratham.py
@@ -1,12 +1,13 @@
 import qrcode
 from PIL import Image
 data = input("ENTER THE DATA YOU WANT TO ENCODE : ")
+save = input("ENTER THE NAME OF YOUR FILE : ")
 qr = qrcode.QRCode(version=1 ,
                    error_correction = qrcode.constants.ERROR_CORRECT_H,
                     box_size = 10 , border = 4 )
 qr.add_data(data)
 qr.make(fit = True)
 image = qr.make_image(fill_color= "black",back_color = "white")
-image.save("qrcode1.png")
+image.save(save)
 print("Generated Successfully !")
 
